082a586d364cbb3421c298f22642c2b36985bed0
chaniel <netqyq@gmail.com>
2016-03-05 13:36:49 +0800

    create Array

---
+++ Array/array.rb
@@ -0,0 +1,8 @@
+# word array, the word can not contains blank character.
+lang = %w(Ruby Perl Python Scheme Pike REBOL)
+p lang
+
+
+# symbol array
+lang = %i(Ruby Perl Python Scheme Pike REBOL)
+p lang
