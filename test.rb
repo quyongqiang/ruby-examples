6d37071735571e851a74f4389963282ac852e8e3
chaniel <netqyq@gmail.com>
2016-04-02 07:55:54 +0800

    add oo

--- test.rb
+++ test.rb
@@ -1,38 +1,9 @@
-class Person
-  attr_accessor :fname
-  attr_accessor :lname
-  attr_accessor :age
+include Math
+p PI
+p sqrt(2)
 
-  # class variable
-  @@haircolors = ['black', 'brown', 'blonde', 'purple', 'red']
-  Var = 1
-
-  # class method
-  def self.haircolors
-    @@haircolors
-  end
-
-  def initialize(f, l, a)
-    @fname = f
-    @lname = l
-    @age = a
-  end
-
-  def to_s
-    "#{@fname} #{@lname}, age #{@age}"
-  end
-
-  def over_the_hill?
-    return false if @age < 40
-    true
-  end
+def hello
+  nil
 end
 
-
-p = Person.new("Brian", "Hoke", 44)
-puts p
-
-puts Person.haircolors
-puts Person::Var
-Person::Var = 2
-puts Person::Var
+p hello
